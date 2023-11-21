e6bd95c2472573c83264049644a783ce08bc22a6
lizafield <143286377+lizafield@users.noreply.github.com>
2023-11-20 21:52:31 -0500

    Initial Setup
    Adding in imports, constants, config, and the app title

--- app.py
+++ app.py
@@ -1,3 +1,61 @@
+import os 
+import pandas ad pd
+import plotly.express as px
 import streamlit as st
-st.title('Final')
-st.write('Hello World!!')
+from dotenv import load_dotenv
+
+from utils.b2 import B2
+
+
+# ------------------------------------------------------
+#                      APP CONSTANTS
+# ------------------------------------------------------
+rural_data = 'Rural_vs_Urban_date.csv'
+
+
+
+# ------------------------------------------------------
+#                        CONFIG
+# ------------------------------------------------------
+load_dotenv()
+
+# load Backblaze connection
+b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
+        key_id=os.environ['B2_KEYID'],
+        secret_key=os.environ['B2_APPKEY'])
+
+
+
+# ------------------------------------------------------
+#                         APP
+# ------------------------------------------------------
+
+st.title('Older Populations Living in Rural Areas')
+
+
+# ------------------------------
+# PART 1 : Filter Data
+# ------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
