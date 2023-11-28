985e227a6bd957499748f64fad4b0d21ccd24bd1
lizafield <143286377+lizafield@users.noreply.github.com>
2023-11-28 15:31:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ rural_data = 'Rural_vs_Urban_date.csv'
 # ------------------------------------------------------
 load_dotenv()
 
-load Backblaze connection
+#load Backblaze connection
 b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
        key_id=os.environ['B2_KEYID'],
        secret_key=os.environ['B2_APPKEY'])
