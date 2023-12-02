1bcc3b467e3ad208172287d19ab437c0a99e17d0
lizafield <143286377+lizafield@users.noreply.github.com>
2023-12-02 14:03:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,3 @@ sel_country = st.selectbox(
 sel_country_df = rural_df.loc[(rural_df['Age group'] != '60+') & (rural_df['Sex'] == "Both sexes") & (rural_df['Residence area'] == 'Rural') & (rural_df['Country' == sel_country])]
 
 st.line_chart(sel_country_df, x = 'Year', y = 'Percentage', color = 'Age group', #color_discrete_sequence = ['#332288','#44AA99','#882255','#CC6677','#DDCC77','#88CCEE'])
-
-
-
