ca12535849766ef05b48f636bd6a7ca8c6e47e45
lizafield <143286377+lizafield@users.noreply.github.com>
2023-12-03 11:57:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -250,23 +250,23 @@ gr_f6_low = f6_low.groupby(['Sex', 'Year'])['Percentage'].mean()
 flat_f6_low = gr_f6_low.reset_index()
 
 
+###GRAPH CREATION###
 
+fig6 = make_subplots(rows=2, cols=2, start_cell="bottom-left")
 
-#fig6 = make_subplots(rows=2, cols=2, start_cell="bottom-left")
+fig6.add_trace(go.line(flat_f6_low, x = 'Year', y = 'Percentage', color = 'Sex'),
+              row = 1, col = 1)
 
-#fig6.add_trace(go.Scatter(x=[1, 2, 3], y=[4, 5, 6]),
-              #row=1, col=1)
+fig6.add_trace(go.line(flat_f6_lowmid, x = 'Year', y = 'Percentage', color = 'Sex'),
+              row = 1, col = 2)
 
-#fig6.add_trace(go.Scatter(x=[20, 30, 40], y=[50, 60, 70]),
-            #  row=1, col=2)
+fig6.add_trace(go.line(flat_f6_upmid, x = 'Year', y = 'Percentage', color = 'Sex'),
+              row = 2, col = 1)
 
-#fig6.add_trace(go.Scatter(x=[300, 400, 500], y=[600, 700, 800]),
-         #     row=2, col=1)
+fig6.add_trace(go.line(flat_f6_high, x = 'Year', y = 'Percentage', color = 'Sex'),
+              row = 2, col = 2)
 
-#fig6.add_trace(go.Scatter(x=[4000, 5000, 6000], y=[7000, 8000, 9000]),
-             # row=2, col=2)
-
-#fig6.show()
+st.plotly_chart(fig6)
 
 
 
