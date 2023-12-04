e74a12ca5d4d062e89362368f7cd2f5531e68ee7
lizafield <143286377+lizafield@users.noreply.github.com>
2023-12-03 19:48:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -272,22 +272,26 @@ axs[0, 0].plot(flat_f6_low_f['Year'], flat_f6_low_f['Percentage'], 'tab:red')
 axs[0, 0].set_title('Low Income')
 axs[0, 1].plot(flat_f6_lowmid_m['Year'], flat_f6_lowmid_m['Percentage'], 'tab:blue')
 axs[0, 1].plot(flat_f6_lowmid_f['Year'], flat_f6_lowmid_f['Percentage'], 'tab:red')
-#axs[0, 1].set_title('Lower Middle Income')
+axs[0, 1].set_title('Lower Middle Income')
 axs[1, 0].plot(flat_f6_upmid_m['Year'], flat_f6_upmid_m['Percentage'], 'tab:blue')
 axs[1, 0].plot(flat_f6_upmid_f['Year'], flat_f6_upmid_f['Percentage'], 'tab:red')
-#axs[1, 0].set_title('Upper Middle Income')
+axs[1, 0].set_title('Upper Middle Income')
 axs[1, 1].plot(flat_f6_high_m['Year'], flat_f6_high_m['Percentage'], 'tab:blue')
 axs[1, 1].plot(flat_f6_high_f['Year'], flat_f6_high_f['Percentage'], 'tab:red')
-#axs[1, 1].set_title('High Income')
+axs[1, 1].set_title('High Income')
+
 
 for ax in axs.flat:
-   ax.set(xlabel='Year', ylabel='Percentage of Population Living Rurally')
+   ax.set(xlabel='Year', ylabel='Percentage')
 
 # Hide x labels and tick labels for top plots and y ticks for right plots.
 #for ax in axs.flat:
     #ax.label_outer()
 
+plt.suptitle('Percentage of Older Males vs Older Females Living Rurally')
+
 st.pyplot(fig=fig_f6)
+
 st.dataframe(flat_f6_high_m)
 
 
